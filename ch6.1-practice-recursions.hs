6d7350b37c24cae9ada97532ea46ed609eb2f257
Theaveas So <theaveas.so@gmail.com>
2023-07-26 12:59:22 +0700

    recursive practice

--- ch6.1-practice-recursions.hs
+++ ch6.1-practice-recursions.hs
@@ -13,3 +13,21 @@ sum' :: Num a => [a] ->  a
 -- sum' []     = 0
 -- sum' (x:xs) = x + sum' xs
 sum' = foldr (+) 0
+
+-- Problems: define a function that drops a given number of elements from the start of a list
+drop' :: Int -> [a] -> [a]
+-- drop' 0 []     = []
+-- drop' n []     = []
+-- drop' 0 (x:xs) = x : xs
+-- drop' n (x:xs) = drop (n-1) xs
+drop' 0 xs = xs
+drop' _ [] = []
+drop' n (_: xs) = drop (n-1) xs
+
+-- Problem: define a function that removes the last element from a non-empty list
+dropLast :: [a] -> [a]
+-- dropLast (x:xs)
+--   | null xs   = []
+--   | otherwise = x : dropLast xs
+dropLast [_] = []
+dropLast (x:xs) = x : dropLast xs
